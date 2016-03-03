7a2e7259ac3101335d76952bc08b2d9219ef4b39
Stanislav Demyanovich <stan@angrybubo.com>
2016-03-03 23:16:36 +0600

    Pair of documenting comments.

--- include/tktk/ecs/System.hpp
+++ include/tktk/ecs/System.hpp
@@ -1,28 +1,35 @@
-/**
-    tktk
+//  tktk
+//
+//  tktk - Copyright (C) 2015 Stanislav Demyanovich <stan@angrybubo.com>
+//
+//  This software is provided 'as-is', without any express or
+//  implied warranty. In no event will the authors be held
+//  liable for any damages arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute
+//  it freely, subject to the following restrictions:
+// 
+//  1.  The origin of this software must not be misrepresented;
+//      you must not claim that you wrote the original software.
+//      If you use this software in a product, an acknowledgment
+//      in the product documentation would be appreciated but
+//      is not required.
+//
+//  2.  Altered source versions must be plainly marked as such,
+//      and must not be misrepresented as being the original software.
+//
+//  3.  This notice may not be removed or altered from any
+//      source distribution.
 
-    tktk - Copyright (C) 2015 Stanislav Demyanovich <stan@angrybubo.com>
 
-    This software is provided 'as-is', without any express or
-    implied warranty. In no event will the authors be held
-    liable for any damages arising from the use of this software.
 
-    Permission is granted to anyone to use this software for any purpose,
-    including commercial applications, and to alter it and redistribute
-    it freely, subject to the following restrictions:
+/// \file
+/// \brief This file provides ecs::System type
+/// \author Stanislav Demyanovich <stan@angrybubo.com>
+/// \date 2015-2016
+/// \copyright tktk is released under the terms of zlib/png license; see full license text at https://opensource.org/licenses/Zlib
 
-    1.  The origin of this software must not be misrepresented;
-        you must not claim that you wrote the original software.
-        If you use this software in a product, an acknowledgment
-        in the product documentation would be appreciated but
-        is not required.
-
-    2.  Altered source versions must be plainly marked as such,
-        and must not be misrepresented as being the original software.
-
-    3.  This notice may not be removed or altered from any
-        source distribution.
-*/
 
 #ifndef TKTK_ECS_SYSTEM_HPP
 #define TKTK_ECS_SYSTEM_HPP
