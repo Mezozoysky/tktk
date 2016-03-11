b3bcd8a5f449fea88491e0aad8444b664086169e
Stanislav Demyanovich <stan@angrybubo.com>
2016-03-11 15:59:51 +0600

    minor.

--- include/tktk/mpool/Id64.hpp
+++ include/tktk/mpool/Id64.hpp
@@ -9,7 +9,7 @@
 //  Permission is granted to anyone to use this software for any purpose,
 //  including commercial applications, and to alter it and redistribute
 //  it freely, subject to the following restrictions:
-// 
+//
 //  1.  The origin of this software must not be misrepresented;
 //      you must not claim that you wrote the original software.
 //      If you use this software in a product, an acknowledgment
@@ -25,9 +25,9 @@
 
 
 /// \file
-/// \brief This file provides mpool::Id64 type
+/// \brief This file provides Id64 type
 /// \author Stanislav Demyanovich <stan@angrybubo.com>
-/// \date 2016
+/// \date 2015-2016
 /// \copyright tktk is released under the terms of zlib/png license; see full license text at https://opensource.org/licenses/Zlib
 
 #include <cstddef>
