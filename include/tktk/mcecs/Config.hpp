c83cf4fe4aa32633e67d659d35ae6457e8e24fa6
Stanislav Demyanovich <stan@angrybubo.com>
2016-06-06 13:00:50 +0600

    mcecs target with fake binary.

---
+++ include/tktk/mcecs/Config.hpp
@@ -0,0 +1,44 @@
+//  tktk
+//
+//  tktk - Copyright (C) 2015 Stanislav Demyanovich <stan@angrybubo.com>
+//
+//  This software is provided 'as-is', without any express or
+//  implied warranty. In no event will the authors be held
+//  liable for any damages arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute
+//  it freely, subject to the following restrictions:
+//
+//  1.  The origin of this software must not be misrepresented;
+//      you must not claim that you wrote the original software.
+//      If you use this software in a product, an acknowledgment
+//      in the product documentation would be appreciated but
+//      is not required.
+//
+//  2.  Altered source versions must be plainly marked as such,
+//      and must not be misrepresented as being the original software.
+//
+//  3.  This notice may not be removed or altered from any
+//      source distribution.
+
+
+/// \file
+/// \brief This file provides configuration defines for tktk-mcecs library
+/// \author Stanislav Demyanovich <stan@angrybubo.com>
+/// \date 2016
+/// \copyright tktk is released under the terms of zlib/png license; see full license text at https://opensource.org/licenses/Zlib
+
+/// \namespace mcecs
+/// \brief Namespace for all the names of tktk-mcecs library
+///
+/// Description stub for `namespace mcecs`
+
+#ifndef TKTK_MCECS_CONFIG_HPP
+#define TKTK_MCECS_CONFIG_HPP
+
+#include <tktk/Config.hpp>
+
+#define TKTK_MCECS_CONFIG_DEFAULT_COMPS_PER_ENTITY 16
+
+#endif // TKTK_MCECS_CONFIG_HPP
